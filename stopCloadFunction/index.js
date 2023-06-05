4fe94fd56627b894871e556e17f0fb04a59223d9
john whately <18461782+GeoDerp@users.noreply.github.com>
2023-06-05 13:20:42 +0930

    init stop index.js

---
+++ stopCloadFunction/index.js
@@ -0,0 +1,29 @@
+// this should not be required if autostop enabled on the container environment variables
+const projectId = '<MYPROJECTID>';
+const zone = 'australia-southeast1-a'
+const instanceName = 'my-mc-server-1'
+
+const functions = require('@google-cloud/functions-framework');
+const compute = require('@google-cloud/compute');
+
+//start async function via http/s request
+functions.http('stopInstancehttp', (req, res) => {
+
+  const instancesClient = new compute.InstancesClient();
+
+  const response = instancesClient.stop({
+    project: projectId,
+    zone,
+    instance: instanceName,
+  });
+  let operation = response.latestResponse;
+
+  // check if operation is complete.
+  if (operation.status == 'DONE') {
+      console.log('Instance stopped.');
+  };
+
+  res.end('OK');
+
+  
+});
