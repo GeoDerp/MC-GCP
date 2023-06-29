09f2f7b33085e5bd71cd36f53b703dbd049b03da
john whately <18461782+GeoDerp@users.noreply.github.com>
2023-06-29 06:31:17 +0000

    fix start script permission issue

--- GCPContainer.tf
+++ GCPContainer.tf
@@ -103,7 +103,7 @@ variable "vm_automatic_restart" {
 
 variable "vm_startupscript" {
   description = "startup script for vm"
-  default = "sudo mkdir -p /home/data && sudo chmod -R u=rwx /home/data && cd /home/data && git clone https://github.com/GeoDerp/MC-GCP && cd MC-GCP && sleep 5 && chmod +x run.sh && sudo bash ./run.sh"
+  default = "sudo mkdir -p /home/data && sudo chmod -R +776 /home/data && cd /home/data && sudo rm -rf MC-GCP && git clone https://github.com/GeoDerp/MC-GCP && cd MC-GCP && sleep 5 && chmod +x run.sh && sudo bash ./run.sh"
 }
 
 variable "restartPolicy" {
